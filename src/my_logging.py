16ab3e4243c467fc4c91e291e4ddbaaf22d131fa
CroissantBaker <sync_canada@protonmail.com>
2023-02-08 12:57:35 -0500

    nothing
    nothing

--- src/my_logging.py
+++ src/my_logging.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # load a csv file
 e_commerce_data_path_csv = "../data/data.csv"
+###
 
 e_commerce_csv_df = pd.read_csv(
     e_commerce_data_path_csv, encoding="unicode_escape", nrows=1000
